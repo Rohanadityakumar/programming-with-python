9c37d3c4d28761403c3dd9c186473fbc0bfacf24
Rohanadityakumar <43025981+Rohanadityakumar@users.noreply.github.com>
2018-10-04 15:13:13 +0530

    Create 19.py

---
+++ 19.py
@@ -0,0 +1,25 @@
+#using function
+x=int(input("enter a number))
+y=int(input("enter another number))
+
+#using add function
+def add():
+       return x+y
+        
+#using sub function
+def sub():
+       return x-y
+
+#using multi function
+def multi():
+        return x*y
+ 
+#using div function
+def div():
+      return x/y
+print("add=")
+z=add()
+print(z)
+print("sub=")
+p=sub()
+        
