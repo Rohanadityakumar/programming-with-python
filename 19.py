f2a63940e0e1002ee5427a21cbe5137f2dcee311
Rohanadityakumar <43025981+Rohanadityakumar@users.noreply.github.com>
2018-10-04 15:34:08 +0530

    Update 19.py

--- 19.py
+++ 19.py
@@ -1,7 +1,8 @@
 #using function
 x=int(input("enter a number))
 y=int(input("enter another number))
-
+O=input("what do u want me to do ROHAN??? is it +,-,*,/")
+            
 #using add function
 def add():
        return x+y
@@ -17,9 +18,17 @@ def multi():
 #using div function
 def div():
       return x/y
-print("add=")
-z=add()
-print(z)
-print("sub=")
-p=sub()
-        
+
+if(o=='+'):
+        res=add()
+            
+elif(o=='-'):
+          res=sub(X,Y)
+
+elif(o=='*'):
+           res==multi(x,y)
+     
+elif(o=='/'):
+           res==div(x,y)
+            
+print(res)
