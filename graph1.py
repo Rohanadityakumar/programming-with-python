174550b6b57eae58e02d4497d8911ceaef44e51b
Rohanadityakumar <43025981+Rohanadityakumar@users.noreply.github.com>
2018-11-03 11:39:09 +0530

    Add files via upload

---
+++ graph1.py
@@ -0,0 +1,9 @@
+#plotting line graph using python
+import matplotlib.pyplot as rohan
+x=[2012,2015,2018]
+y=[66,75,90]
+rohan.plot(x,y)
+rohan.xlabel("X Axis")
+rohan.ylabel("Y Axis")
+rohan.title("Rise in price of petrol in BLR")
+rohan.show()
